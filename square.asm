b404d208e453c94a5c7f029f346738c20ad4ec88
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-11-07 22:42:21 +0100

    Update square.asm

--- square.asm
+++ square.asm
@@ -1,16 +1,16 @@
-%bootloader
+	%bootloader
 %name epic.img
 
 mov    ax,h0013        
 int    h10
 push   ha000
-push   ha000
 pop    ds
-pop    es
+
+%label square
 mov    dl,20
 
 %label line2
-mov    cl,20
+mov    cl,20	
 
 %label line
 mov    *bx,ax
@@ -20,4 +20,37 @@ add    bx,300
 sub    dl,1
 cmp    dl,0
 jne    line2
+push   ax
+mov    dl,*hff41
+
+%label input
 hlt
+in     al,h60
+cmp    al,dl
+je     input
+xor    dl,dl	
+cmp    al,h91
+je     wkey
+jmp    input
+
+%label wkey
+mov    *hff41,al
+mov    al,*hff40
+add    al,1
+mov    *hff40,al
+mul    ax,320
+mov    bx,ax
+pop    ax
+jmp    square
+
+
+
+
+
+
+
+
+
+
+
+
