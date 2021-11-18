fcc9db386e9e66eefacfecc08b270ebe6b5ee1bf
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-11-18 17:15:46 +0100

    Update consolegame.asm

--- consolegame.asm
+++ consolegame.asm
@@ -2,13 +2,9 @@
 %name epic
 
 %lib KERNEL32
-%func ExitProcess
 %func GetStdHandle
 %func WriteConsoleA
 %func SetConsoleCursorPosition
-%func SetConsoleCursorInfo
-%func SetCurrentConsoleFontEx
-%func SetConsoleWindowInfo
 %func Sleep
 
 %lib USER32
@@ -16,42 +12,15 @@
 
 %var map "###########\n#         #\n#         #\n#         #\n#         #\n#    @    #\n#         #\n#         #\n#         #\n#         #\n###########"
 %var handle 8 0
-%var player 4 0
+%var size 1 0
+%var dir 1 1
+%var wurm 10 65
 
-%var cbsize 4 84
-%var nFont 4 0
-%var xfont 2 36
-%var yfont 2 36
-%var meuk 0 0
 
-%var rLeft 2 0
-%var rTop 2 0
-%var rRight 2 10
-%var rBottom 2 10
-
-mov    eax,$map
-add    eax,65
-mov    $player,eax
 push   hf5
 call   GetStdHandle
 mov    $handle,eax
-mov    eax,$cbsize
-push   eax
-push   0
-mov    eax,*$handle
-push   eax
-call   SetCurrentConsoleFontEx
-mov    eax,$rLeft
-push   eax
-push   1
-mov    eax,*$handle
-push   eax
-call   SetConsoleWindowInfo
-mov    eax,$cbsize
-push   eax
-mov    eax,*$handle
-push   eax
-call   SetConsoleCursorInfo
+call   rndfood
 
 %label print
 push   0
@@ -61,93 +30,101 @@ push   $map
 mov    eax,*$handle
 push   eax
 call   WriteConsoleA
-push   0
+mov    eax,0
+push   eax
 mov    eax,*$handle
 push   eax
 call   SetConsoleCursorPosition
-
-%label inp
 push   300
 call   Sleep
 push   h57
 call   GetKeyState
 test   al,128
 jne    wkey
-push   h41
-call   GetKeyState
-test   al,128
-jne    akey
-push   h44
-call   GetKeyState
-test   al,128
-jne    dkey
 push   h53
 call   GetKeyState
 test   al,128
 jne    skey
-mov    ecx,*$rLeft
+push   h44
+call   GetKeyState
+test   al,128
+jne    dkey
+push   h41
+call   GetKeyState
+test   al,128
+jne    akey
+xor    cl,cl
+mov    cl,*$dir
 jmp    movement
-mov    cl,50
-call   rnd
+
+%label rndfood
+rdrand eax
+and    eax,255
+cmp    eax,131
+ja     rndfood
 mov    ebx,$map
 add    ebx,eax
-mov    al,70
-mov    *ebx,al
-
-%label rnd
-jmp    exit
-rdrand eax
-
-%label L01
-cmp    eax,ecx
-jg     L00
+mov    al,32
+cmp    *ebx,al
+jne    rndfood
+mov    dl,70
+mov    *ebx,dl
 ret
-
-%label L00
-shr    eax
-jmp    L01
-
-%label skey
-mov    cl,12
-jmp    movement
-
-%label akey
-mov    cl,hff
-jmp    movement
 
 %label wkey
 mov    cl,hf4
 jmp    movement
 
+%label skey
+mov    cl,12
+jmp    movement
+
 %label dkey
 mov    cl,1
+jmp    movement
+
+%label akey
+mov    cl,hff
 
 %label movement
-mov    eax,*$player
-mov    bl,33
-mov    *eax,bl
+mov    ebx,$map
+mov    al,*$wurm
+add    bl,al
 add    al,cl
-mov    dl,35
-cmp    dl,*eax
+mov    $wurm,al
+mov    al,32
+mov    *ebx,al
+add    bl,cl
+mov    al,35
+cmp    *ebx,al
 je     exit
-mov    bl,64
-mov    *eax,bl
-mov    $player,eax
-mov    $rLeft,ecx
-jmp    print
+mov    al,70
+cmp    *ebx,al
+jne    con
+mov    edx,*$size
+inc    edx
+mov    $size,edx
+push   ebx
+call   rndfood
+pop    ebx
+
+%label con
+mov    al,64
+mov    *ebx,al
+mov    $dir,cl
+mov    ebx,$wurm
+mov    cl,*$size
+add    bl,cl
+
+%label update
+cmp    cl,0
+je     print
+dec    bl
+mov    al,*ebx
+inc    bl
+mov    *ebx,al
+dec    bl
+dec    cl
+jmp    update
 
 %label exit
-call   ExitProcess
-
-
-
-
-
-
-
-
-
-
-
-
-
