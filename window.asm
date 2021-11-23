af615600d81f2292c864b667400d3b3c9deb4024
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-11-23 13:11:24 +0100

    Update window.asm

--- window.asm
+++ window.asm
@@ -1,60 +1,112 @@
+
 %window
 %name epic
 
-%library USER32
-%function MessageBoxA
-%function CreateWindowExA
-%function RegisterClassExA
-%function DefWindowProcA
+%lib   KERNEL32
+%func  GetModuleHandleA
+%func  ExitProcess
+%func  GetLastError
+%func  WriteConsoleA
+%func  CreateFileA
+%func  CloseHandle
 
-%library KERNEL32
-%function GetModuleHandleA
+%lib   USER32
+%func  RegisterClassA
+%func  CreateWindowExA
+%func  DefWindowProcA
+%func  ShowWindow
+%func  MessageBoxA
 
-%variable mes "hello world!"
+%var   style         4 0
+%var   lpfnWndProc   4 0
+%var   cbClsExtra    4 0
+%var   cbWndExtra    4 0
+%var   hInstance     4 0
+%var   hIcon         4 0
+%var   hCursor       4 0
+%var   hbrBackground 4 0
+%var   lpszMenuName  4 0
+%var   lpszClassName 4 0
 
-%variable class "class"
+%var   hwnd          4 0
+%var   handle        4 0
 
-%variable cbsize 4 0
-%variable style 4 0
-%variable proc 4 0
-%variable m1 4 0
-%variable m2 4 0
-%variable hinstance 4 0
-%variable icon 4 0
-%variable cursor 4 0
-%variable background 4 0
-%variable menuname 4 0
-%variable clasname 4 0
-%variable icn 4 0
+%var   name1 "window"
+%var   name2 "debug.txt"
 
-mov    al,52
-mov    $cbsize,eax
-mov    eax,$class
-mov    $clasname,eax
-mov    eax,~proc
-mov    $proc,eax
-push   $cbsize
-call   RegisterClassExA 
 push   0
-call   GetModuleHandleA
+push   h80
+push   1
 push   0
+push   1
+push   h40000000
+push   $name2
+call   CreateFileA
 push   eax
+call   CloseHandle
 push   0
 push   0
-push   40
-push   40
-push   40
-push   40
+push   0
+push   0
+call   MessageBoxA
+push   0
+call   GetModuleHandleA 
+mov    $hInstance,eax
+mov    eax,$name1
+mov    $lpszMenuName,eax
+mov    $lpszClassName,eax
+mov    eax,~proc
+mov    $lpfnWndProc,eax
+push   $style
+call   RegisterClassA
+cmp    eax,0
+je     exit
+push   0
+push   $hInstance
+push   0
+push   0
+push   h40
+push   h40
+push   h40
+push   h40
 push   h10000000
-push   $mes
-push   $class
+push   $lpszClassName
+push   $lpszClassName
 push   0
 call   CreateWindowExA
+mov    $hwnd,eax
+call   GetLastError
+mov    $style,eax
+
+%label inf
+jmp    inf
+
+%label exit
 ret
 
 %label proc
+push   0
+call   ExitProcces
+push   0
+push   0
+push   0
+push   $hwnd
 call   DefWindowProcA
 ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
